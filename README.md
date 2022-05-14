3aa76205c61cb53ff3c230ab7e1cbdc7ef86fee2
John Eisenmann <esperantanaso@gmail.com>
2022-05-14 17:41:39 -0400

    Clean up style

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# WheatSystem Specification
+
+This repository contains resources to generate documentation for WheatSystem. The generated documentation is hosted on [this webpage](http://www.ostracodfiles.com/wheatsystem/menu.html).
+
+## Usage
+
+This project has the following system-wide dependencies:
+
+* Node.js version 16 or later
+
+To generate the documentation:
+
+```
+node ./generate.js
+```
+
+
--- generate.js
+++ generate.js
@@ -1,7 +1,7 @@
 
 const fs = require("fs");
 
-let categoryList = [];
+const categoryList = [];
 
 class Category {
     
@@ -14,16 +14,15 @@ class Category {
     toJson() {
         return {
             name: this.name,
-            instructionList: this.instructionList.map(instruction => instruction.toJson())
+            instructionList: this.instructionList.map((instruction) => instruction.toJson()),
         };
     }
     
     toHtml() {
-        let tempHtmlList = this.instructionList.map(instruction => instruction.toHtml())
+        const htmlList = this.instructionList.map((instruction) => instruction.toHtml())
         return `<p class="title2">${this.name}</p>
-${tempHtmlList.join("\n")}`;
+${htmlList.join("\n")}`;
     }
-    
 }
 
 class Instruction {
@@ -41,32 +40,31 @@ class Instruction {
             name: this.name,
             opcode: this.opcode,
             description: this.description,
-            argumentList: this.argumentList.map(argument => argument.toJson()),
-            noteList: this.noteList
+            argumentList: this.argumentList.map((argument) => argument.toJson()),
+            noteList: this.noteList,
         };
     }
     
     toHtml() {
-        let tempOpcodeText = this.opcode.toString(16).toUpperCase();
-        while (tempOpcodeText.length < 2) {
-            tempOpcodeText = "0" + tempOpcodeText;
+        let opcodeText = this.opcode.toString(16).toUpperCase();
+        while (opcodeText.length < 2) {
+            opcodeText = "0" + opcodeText;
         }
-        tempOpcodeText = "0x" + tempOpcodeText;
-        let tempNameList = this.argumentList.map(argument => argument.name)
-        let tempHtmlList = this.argumentList.map(argument => argument.toHtml())
-        for (let note of this.noteList) {
-            tempHtmlList.push(`<li>${note}</li>`);
+        opcodeText = "0x" + opcodeText;
+        const nameList = this.argumentList.map((argument) => argument.name)
+        const htmlList = this.argumentList.map((argument) => argument.toHtml())
+        for (const note of this.noteList) {
+            htmlList.push(`<li>${note}</li>`);
         }
-        return `<p><span class="code">${this.name} ${tempNameList.join(", ")}</span></p>
+        return `<p><span class="code">${this.name} ${nameList.join(", ")}</span></p>
 <div class="description">
     <p>${this.description}</p>
     <ul>
-        <li><span class="code">${this.name}</span> opcode = <span class="code">${tempOpcodeText}</span></li>
-${tempHtmlList.join("\n")}
+        <li><span class="code">${this.name}</span> opcode = <span class="code">${opcodeText}</span></li>
+${htmlList.join("\n")}
     </ul>
 </div>`;
     }
-    
 }
 
 class Argument {
@@ -79,99 +77,105 @@ class Argument {
     toJson() {
         return {
             name: this.name,
-            description: this.description
+            description: this.description,
         };
     }
     
     toHtml() {
         return `<li><span class="code">${this.name}</span> = ${this.description}</span></li>`;
     }
-    
 }
 
-function splitEqualityStatement(statement) {
-    let tempTextList = statement.split("=");
-    return tempTextList.map(text => text.trim());
-}
+const splitEqualityStatement = (statement) => {
+    const textList = statement.split("=");
+    return textList.map((text) => text.trim());
+};
 
-function parseInstructions(instructionText) {
+const parseInstructions = (instructionText) => {
     let currentCategory = null;
     let currentInstruction = null;
-    let lineList = instructionText.split("\n");
-    for (let line of lineList) {
+    const lineList = instructionText.split("\n");
+    for (const line of lineList) {
         if (line.length <= 0) {
             continue;
         }
-        let tempCharacter = line.charAt(0);
-        let tempText = line.substring(2, line.length);
-        if (tempCharacter === "!") {
-            currentCategory = new Category(tempText);
+        const character = line.charAt(0);
+        const text = line.substring(2, line.length);
+        if (character === "!") {
+            currentCategory = new Category(text);
         }
-        if (tempCharacter === "$") {
-            let [tempName, tempOpcode] = splitEqualityStatement(tempText);
-            tempOpcode = tempOpcode.substring(2, tempOpcode.length);
-            tempOpcode = parseInt(tempOpcode, 16);
-            currentInstruction = new Instruction(tempName, tempOpcode);
+        if (character === "$") {
+            let [name, opcode] = splitEqualityStatement(text);
+            opcode = opcode.substring(2, opcode.length);
+            opcode = parseInt(opcode, 16);
+            currentInstruction = new Instruction(name, opcode);
             currentCategory.instructionList.push(currentInstruction);
         }
-        if (tempCharacter === "#") {
-            currentInstruction.description = tempText;
+        if (character === "#") {
+            currentInstruction.description = text;
         }
-        if (tempCharacter === ">") {
-            let [tempName, tempDescription] = splitEqualityStatement(tempText);
-            let tempArgument = new Argument(tempName, tempDescription);
-            currentInstruction.argumentList.push(tempArgument);
+        if (character === ">") {
+            const [name, description] = splitEqualityStatement(text);
+            const argument = new Argument(name, description);
+            currentInstruction.argumentList.push(argument);
         }
-        if (tempCharacter === "*") {
-            currentInstruction.noteList.push(tempText);
+        if (character === "*") {
+            currentInstruction.noteList.push(text);
         }
     }
-}
+};
 
-function populateTemplatePlaceholders(templateText, instructionHtml) {
+const populateTemplatePlaceholders = (templateText, instructionHtml) => {
     let output = templateText;
-    var tempDate = new Date();
-    output = output.replace("{TIMESTAMP}", tempDate.toLocaleDateString("en-US", {
+    const date = new Date();
+    output = output.replace("{TIMESTAMP}", date.toLocaleDateString("en-US", {
         year: "numeric",
         month: "numeric",
         day: "numeric",
         hour: "numeric",
         minute: "2-digit",
-        timeZoneName: "short"
+        timeZoneName: "short",
     }));
     output = output.replace("{INSTRUCTIONS}", instructionHtml);
     return output;
-}
+};
 
-function formatHtmlStyle(documentHtml) {
-    let tempOldList = documentHtml.split("`");
-    let tempNewList = [];
-    for (let index in tempOldList) {
-        let tempText = tempOldList[index];
+const formatHtmlStyle = (documentHtml) => {
+    const oldList = documentHtml.split("`");
+    const newList = [];
+    oldList.forEach((text, index) => {
         if (index > 0) {
             if (index % 2 == 1) {
-                tempNewList.push(`<span class="code">`);
+                newList.push(`<span class="code">`);
             } else {
-                tempNewList.push(`</span>`);
+                newList.push(`</span>`);
             }
         }
-        tempNewList.push(tempText);
-    }
-    return tempNewList.join("");
-}
+        newList.push(text);
+    });
+    return newList.join("");
+};
 
 console.log("Generating documentation...");
 
-let templateText = fs.readFileSync("./bytecodeTemplate.html", "utf8");
-let instructionText = fs.readFileSync("./bytecodeInstructions.txt", "utf8");
+const templateText = fs.readFileSync("./bytecodeTemplate.html", "utf8");
+const instructionText = fs.readFileSync("./bytecodeInstructions.txt", "utf8");
 parseInstructions(instructionText);
-let instructionHtml = categoryList.map(category => category.toHtml()).join("\n");
+const instructionHtml = categoryList.map((category) => category.toHtml()).join("\n");
 let documentHtml = populateTemplatePlaceholders(templateText, instructionHtml);
 documentHtml = formatHtmlStyle(documentHtml);
-let instructionsJson = categoryList.map(category => category.toJson());
+const instructionsJson = categoryList.map((category) => category.toJson());
 
-fs.writeFileSync("./bytecode.html", documentHtml);
-fs.writeFileSync("./bytecodeInstructions.json", JSON.stringify(instructionsJson));
+const fileContentMap = {
+    "./bytecode.html": documentHtml,
+    "./bytecodeInstructions.json": JSON.stringify(instructionsJson),
+};
+
+for (const path in fileContentMap) {
+    const content = fileContentMap[path];
+    console.log(`Writing file to ${path}...`);
+    fs.writeFileSync(path, content);
+}
 
 console.log("Finished.");
 
